3449daae5e5978d53b8b3035dc46a793eda0fe20
Pavel Bazin <pvlbzn@gmail.com>
2017-02-11 18:20:21 +0100

    PyPi publication

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='gobject',
+    description='Pythonic Google Geocode API wrapper',
+    version='0.1.0',
+    url='https://github.com/pvlbzn/gobject',
+    author='Pavel Bazin',
+    author_email='pvlbzn@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='google geocode api wrapper',
+    packages=['gobject'])
